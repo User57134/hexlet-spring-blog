367649c61d43f6164d3e4f3485eccd6fc9394d77
Player One <pl17dev@gmail.com>
2026-09-21 15:11:38 +0300

    Урок 2 'Знакомстов со Spring Boot': initial commit

---
+++ src/main/java/io/hexlet/HomeController.java
@@ -0,0 +1,13 @@
+package io.hexlet;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Добро пожаловать в Hexlet Spring Blog!";
+    }
+}
