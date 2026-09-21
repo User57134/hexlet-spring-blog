4201c1e6a2a673f3e056b1de4ec8613b1407f5b3
Player One <pl17dev@gmail.com>
2026-09-21 16:20:47 +0300

    Урок 3 'Аннотации': Добавлен обработчик для эндпоинта /about

--- src/main/java/io/hexlet/HomeController.java
+++ src/main/java/io/hexlet/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home() {
         return "Добро пожаловать в Hexlet Spring Blog!";
     }
+
+    @GetMapping("/about")
+    public String about() {
+        return "Проект для изучения фреймворка springboot!";
+    }
 }
